8c33f298598d0596f93af7b79b22707f9fb999bd
21giulio21@gmail.com <21giulio21@gmail.com>
2018-04-29 14:56:28 +0200

    d

--- AutofillLogin_2(FAKE)/app/src/main/java/com/example/instagram/NewActivity.java
+++ AutofillLogin_2(FAKE)/app/src/main/java/com/example/instagram/NewActivity.java
@@ -1,4 +1,4 @@
-package kkkkkk.kkkkk;
+package kk.kk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
--- AutofillLogin_2(FAKE)/app/src/main/java/com/instagram/MainActivity.java
+++ AutofillLogin_2(FAKE)/app/src/main/java/com/instagram/MainActivity.java
@@ -1,4 +1,4 @@
-package kkkkkk.kkkkk;
+package kk.kk;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
